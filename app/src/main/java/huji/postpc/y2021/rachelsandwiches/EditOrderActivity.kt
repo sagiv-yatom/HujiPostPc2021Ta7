d07eb5bed53d311d8600d391b77b96875833c69b
sagiv-yatom <sagiv.yatom@cs.huji.ac.il>
2021-06-13 14:56:58 +0300

    improve EditOrderActivity

--- app/src/main/java/huji/postpc/y2021/rachelsandwiches/EditOrderActivity.kt
+++ app/src/main/java/huji/postpc/y2021/rachelsandwiches/EditOrderActivity.kt
@@ -1,6 +1,7 @@
 package huji.postpc.y2021.rachelsandwiches
 
 import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,7 @@ import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.DocumentSnapshot
+import java.util.*
 
 class EditOrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,7 @@ class EditOrderActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_order)
 
         val database = FirestoreData(this)
+        val orderId = database.orderId.toString()
 
         val customerNameEditText = findViewById<EditText>(R.id.customerNameEditText)
         val picklesNumTextView = findViewById<TextView>(R.id.picklesNumTextView)
@@ -24,24 +27,88 @@ class EditOrderActivity : AppCompatActivity() {
         val hummusButton = findViewById<Button>(R.id.hummusButton)
         val tahiniButton = findViewById<Button>(R.id.tahiniButton)
         val commentEditText = findViewById<EditText>(R.id.commentEditText)
+        val saveOrderButton = findViewById<FloatingActionButton>(R.id.saveOrderButton)
 
-        database.db.collection("orders").document(database.orderId.toString()).get()
+        var name: String = ""
+        var pickles: Int = 0;
+        var hummus: Boolean = false
+        var tahini: Boolean = false
+        var comment: String = ""
+
+        database.db.collection("orders").document(orderId).get()
             .addOnSuccessListener { result: DocumentSnapshot ->
                 Log.d(ContentValues.TAG, "Document successfully downloaded")
                 val downloadedOrder = result.toObject(Order::class.java)
                 if (downloadedOrder != null) {
-                    customerNameEditText.setText(downloadedOrder.customerName)
-                    picklesNumTextView.text = downloadedOrder.pickles.toString()
-
-                    if (downloadedOrder.hummus) hummusButton.text = "yes" else {
-                        hummusButton.text = "no"
+                    /** name */
+                    name = downloadedOrder.customerName
+                    customerNameEditText.setText(name)
+                    /** pickles */
+                    pickles = downloadedOrder.pickles
+                    picklesNumTextView.text = pickles.toString()
+                    decreasePicklesNumButton.isEnabled = pickles != 0
+                    increasePicklesNumButton.isEnabled = pickles != 10
+                    /** hummus */
+                    if (downloadedOrder.hummus) {
+                        hummus = true
+                        hummusButton.text = "YES"
+                    } else {
+                        hummus = false
+                        hummusButton.text = "NO"
                     }
-                    if (downloadedOrder.tahini) tahiniButton.text = "yes" else {
-                        tahiniButton.text = "no"
+                    /** tahini */
+                    if (downloadedOrder.tahini) {
+                        tahini = true
+                        tahiniButton.text = "YES"
+                    } else {
+                        tahini = false
+                        tahiniButton.text = "NO"
                     }
-                    commentEditText.setText(downloadedOrder.comment)
+                    /** comment */
+                    comment = downloadedOrder.comment
+                    commentEditText.setText(comment)
                 }
             }
             .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error downloading document", e) }
+
+        decreasePicklesNumButton.setOnClickListener {
+            pickles--
+            picklesNumTextView.text = pickles.toString()
+            decreasePicklesNumButton.isEnabled = pickles != 0
+            increasePicklesNumButton.isEnabled = pickles != 10
+        }
+
+        increasePicklesNumButton.setOnClickListener {
+            pickles++
+            picklesNumTextView.text = pickles.toString()
+            decreasePicklesNumButton.isEnabled = pickles != 0
+            increasePicklesNumButton.isEnabled = pickles != 10
+        }
+
+        hummusButton.setOnClickListener {
+            if (hummus) {
+                hummus = false
+                hummusButton.text = "NO"
+            } else {
+                hummus = true
+                hummusButton.text = "YES"
+            }
+        }
+
+        tahiniButton.setOnClickListener {
+            if (tahini) {
+                tahini = false
+                tahiniButton.text = "NO"
+            } else {
+                tahini = true
+                tahiniButton.text = "YES"
+            }
+        }
+
+        saveOrderButton.setOnClickListener {
+            name = customerNameEditText.text.toString()
+            comment = commentEditText.text.toString()
+            database.uploadDocument(orderId, name, pickles, hummus, tahini, comment)
+        }
     }
 }
--- app/src/main/java/huji/postpc/y2021/rachelsandwiches/FirestoreData.kt
+++ app/src/main/java/huji/postpc/y2021/rachelsandwiches/FirestoreData.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -30,7 +31,7 @@ class FirestoreData(context: Context) {
         val newOrder = Order(
             id, customerName, pickles, hummus, tahini, comment, "waiting"
         )
-        db.collection("orders").document(id).set(newOrder)
+        db.collection("orders").document(id).set(newOrder, SetOptions.merge())
             .addOnSuccessListener { Log.d(TAG, "New order successfully uploaded") }
             .addOnFailureListener { e -> Log.w(TAG, "Error uploading new order", e) }
 
