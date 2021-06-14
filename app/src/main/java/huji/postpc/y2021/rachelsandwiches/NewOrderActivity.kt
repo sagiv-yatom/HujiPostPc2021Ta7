f73247c97efe9fd2b049b5eb85f00fc165ad55fe
sagiv-yatom <sagiv.yatom@cs.huji.ac.il>
2021-06-14 14:10:08 +0300

    make screens scrollable

--- app/src/main/java/huji/postpc/y2021/rachelsandwiches/NewOrderActivity.kt
+++ app/src/main/java/huji/postpc/y2021/rachelsandwiches/NewOrderActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -83,4 +84,73 @@ class NewOrderActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        val customerNameEditText = findViewById<EditText>(R.id.customerNameEditText)
+        val picklesNumTextView = findViewById<TextView>(R.id.picklesNumTextView)
+        val hummusButton = findViewById<Button>(R.id.hummusButton)
+        val tahiniButton = findViewById<Button>(R.id.tahiniButton)
+        val commentEditText = findViewById<EditText>(R.id.commentEditText)
+
+        val name = customerNameEditText.text.toString()
+        val pickles = Integer.valueOf(picklesNumTextView.text.toString())
+        var hummus = false
+        if (hummusButton.text.toString() == "YES") {
+            hummus = true
+        }
+        var tahini = false
+        if (tahiniButton.text.toString() == "YES") {
+            tahini = true
+        }
+        val comment = commentEditText.text.toString()
+
+        outState.run {
+            putString("customer name", name)
+            putInt("pickles", pickles)
+            putBoolean("hummus", hummus)
+            putBoolean("tahini", tahini)
+            putString("comment", comment)
+        }
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val name = savedInstanceState.getString("customer name")
+        val pickles = savedInstanceState.getInt("pickles")
+        val hummus = savedInstanceState.getBoolean("hummus")
+        val tahini = savedInstanceState.getBoolean("tahini")
+        val comment = savedInstanceState.getString("comment")
+
+        val customerNameEditText = findViewById<EditText>(R.id.customerNameEditText)
+        val picklesNumTextView = findViewById<TextView>(R.id.picklesNumTextView)
+        val hummusButton = findViewById<Button>(R.id.hummusButton)
+        val tahiniButton = findViewById<Button>(R.id.tahiniButton)
+        val commentEditText = findViewById<EditText>(R.id.commentEditText)
+
+        customerNameEditText.setText(name)
+        picklesNumTextView.text = pickles.toString()
+
+        if (hummus) {
+            hummusButton.text = "YES"
+        }
+        else {
+            hummusButton.text = "NO"
+        }
+
+        if (tahini) {
+            tahiniButton.text = "YES"
+        }
+        else {
+            tahiniButton.text = "NO"
+        }
+
+        commentEditText.setText(comment)
+
+    }
+
 }
